db8f8797a9031dfa156fac56adf3eca3b6ead8fc
fareezjusman <fariz.jusman@gmail.com>
2020-12-07 21:08:44 +0800

    2108

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -2,8 +2,8 @@ function haversine(lat1, lon1, lat2, lon2) {
   let R = 6371; //earth radius in km
   let dLat = (lat2 - lat1).toRad();
   let dLon = (lon2 - lon1).toRad();
-  let lat1 = lat1.toRad();
-  let lat2 = lat2.toRad();
+  lat1 = lat1.toRad();
+  lat2 = lat2.toRad();
 
   let a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
@@ -29,7 +29,7 @@ function roundVal(val) {
 function calculateDistance(lat, lon) {
   let d1 = haversine(lat, lon, 2.922562, 101.650965); //depulze cyberjaya
   let d2 = haversine(lat, lon, 3.073065, 101.607787); //sunway pyramid
-  let d1 = haversine(lat, lon, 3.158761, 101.714524); //KLCC
+  let d3 = haversine(lat, lon, 3.158761, 101.714524); //KLCC
 
   return [d1, d2, d3];
 }
