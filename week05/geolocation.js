72e6303b69fb698c61c47e26e785f0004b19b5c7
fareezjusman <fariz.jusman@gmail.com>
2020-12-07 20:27:05 +0800

    change directory

--- week05/geolocation.js
+++
@@ -1,8 +0,0 @@
-let elLocate = document.getElementById("locate");
-elLocate.addEventListener("click", function () {
-  let elLate = document.getElementById("lat");
-  let elLong = document.getElementById("long");
-
-  elLate.innerHTML = "Latitude : " + position.coords.latitude;
-  elLong.innerHTML = "Longitude : " + position.coords.latitude;
-});
