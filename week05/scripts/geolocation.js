d0c624c7cee3f4282cbe4a04b8b99877f8848ba4
fareezjusman <fariz.jusman@gmail.com>
2020-12-07 20:33:13 +0800

    adjust 2033

--- week05/scripts/geolocation.js
+++ week05/scripts/geolocation.js
@@ -1,8 +1,14 @@
 let elLocate = document.getElementById("locate");
 elLocate.addEventListener("click", function () {
-  let elLate = document.getElementById("lat");
-  let elLong = document.getElementById("long");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let elLate = document.getElementById("lat");
+      let elLong = document.getElementById("long");
 
-  elLate.innerHTML = "Latitude : " + position.coords.latitude;
-  elLong.innerHTML = "Longitude : " + position.coords.latitude;
+      elLate.innerHTML = "Latitude : " + position.coords.latitude;
+      elLong.innerHTML = "Longitude : " + position.coords.latitude;
+    });
+  } else {
+    alert("Geolocation is not supported by this browser");
+  }
 });
