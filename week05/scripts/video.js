015f0cfa57f2a7828c188c837c89c4241ed7a7ec
fareezjusman <fariz.jusman@gmail.com>
2020-12-08 20:43:55 +0800

    2043

--- week05/scripts/video.js
+++ week05/scripts/video.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   //window.onload scripts tag dekat head tag
   let video = document.getElementById("myVideo");
+
   let playBtn = document.getElementById("playBtn");
   playBtn.addEventListener("click", function () {
     video.play();
@@ -9,5 +10,10 @@ window.onload = function () {
   let pauseBtn = document.getElementById("pauseBtn");
   pauseBtn.addEventListener("click", function () {
     video.pause();
+  });
+
+  let freezeBtn = document.getElementById("freezeBtn");
+  freezeBtn.addEventListener("click", function () {
+    video.freeze();
   });
 };
