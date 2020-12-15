5b7b7e8f40e810be676bacbedd51fa24997d9072
fareezjusman <fariz.jusman@gmail.com>
2020-12-15 20:29:03 +0800

    2028

---
+++ week06/scripts/fetch.js
@@ -0,0 +1,10 @@
+function GetBookings() {
+  let url =
+    "https://api.sheety.co/21f68e939f7567bfd0d166e36b99e2c2/bookingApp/bookings";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      // Do something with the data
+      console.log(json.bookings);
+    });
+}
