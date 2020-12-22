a15ffdfb34b3c887aeddcd39a4919cda75da16f5
fareezjusman <fariz.jusman@gmail.com>
2020-12-22 20:23:11 +0800

    2023

--- tableapp/scripts/book.js
+++ tableapp/scripts/book.js
@@ -18,7 +18,7 @@ function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
   })
     .then((response) => response.json())
     .then((json) => {
-      alert(json.booking.name + " added!");
+      alert(json.bookings.name + " added!");
     });
 }
 
