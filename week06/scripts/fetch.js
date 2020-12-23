6e3eeaf32fc6102eba2c3ac4c115239aedd727b6
fareezjusman <fariz.jusman@gmail.com>
2020-12-23 11:07:25 +0800

    2007

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -8,7 +8,12 @@ function GetBookings() {
       console.log(json.bookings);
       let bookingList = document.getElementById("bookingList");
       bookingList.innerHTML = "";
-      for (let i = 0; i < json.bookings.lenght; i++) {
+
+      let jsonDiv = document.getElementById("json");
+      jsonDiv.innerHTML = "";
+      jsonDiv.innerHTML = json.bookings;
+
+      for (let i = 0; i < json.bookings.length; i++) {
         let gName = json.bookings[i].name;
         let gEmail = json.bookings[i].email;
         let gPax = json.bookings[i].pax;
@@ -23,4 +28,58 @@ function GetBookings() {
 let getBookingBtn = document.getElementById("getBooking");
 getBookingBtn.addEventListener("click", function () {
   GetBookings();
+});
+
+function BookNow(guestName, guestEmail, guestPax) {
+  let url =
+    "https://api.sheety.co/21f68e939f7567bfd0d166e36b99e2c2/bookingApp/bookings";
+  let body = {
+    booking: {
+      name: guestName,
+      email: guestEmail,
+      pax: guestPax,
+    },
+  };
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      // Do something with object
+      //console.log(json.booking);
+      let bookMsg = document.getElementById("bookMsg");
+      bookMsg.innerHTML = json.booking.name + " added!";
+      GetBookings();
+    });
+}
+
+let bookNow = document.getElementById("bookNow");
+bookNow.addEventListener("click", function () {
+  let gName = document.getElementById("guestName").value;
+  let gEmail = document.getElementById("guestEmail").value;
+  let gPax = document.getElementById("guestPax").value;
+
+  BookNow(gName, gEmail, gPax);
+});
+
+function DeleteBooking(id) {
+  let url =
+    "https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/bookingApp/bookings/" +
+    id;
+  fetch(url, {
+    method: "DELETE",
+  }).then((response) => {
+    document.getElementById("deleteMsg").innerHTML = "Booking Deleted!";
+    GetBookings();
+  });
+}
+
+let deleteBooking = document.getElementById("deleteBooking");
+deleteBooking.addEventListener("click", function () {
+  let deleteId = document.getElementById("deleteId").value;
+  DeleteBooking(deleteId);
 });
